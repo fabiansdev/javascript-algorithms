3a9752bb87826c77a559d09837cb8f6eaece79a3
fabiansdev <fabian.sarmiento@protonmail.com>
2017-07-19 10:00:07 -0500

    First commit

---
+++ solve-me-first/app.js
@@ -0,0 +1,12 @@
+function solveMeFirst(a, b){
+  return a+b
+}
+
+function main(){
+  // call readLine() to read a line
+  // use console.log() to write to stdout
+  var a = parseInt(readLine());
+  var b = parseInt(readLine());
+  var res = solveMeFirst(a,b);
+  console.log(res);
+}
